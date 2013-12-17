7f82eeed62301d32d66095e5e39d3bdb42a396bf
xupingmao <xupingmao@gmail.com>
2013-12-17 14:07:12 +0800

    添加了按钮和音乐

---
+++ js/buttons.js
@@ -0,0 +1,23 @@
+var NormalBtn = function(_id, _text, _x, _y){
+	var _btn = new Q.Button({id:_id, image:Q.getDOM("button"), 
+	x:_x, y:_y, width:99, height:38,
+	scaleX:2,
+	scaleY:2,
+	up:{rect:[0,0,99,38]},
+	over:{rect:[0,38,99,38]},
+	down:{rect:[0,38,99,38]},
+	disabled:{rect:[0,0,99,38]}
+	});
+	var _text = new Q.Text({
+		font:"20px arial",
+		text:_text,
+		textAlign:"center",
+		lineWidth:100,
+		y:5
+	});
+	_btn.addChild(_text);
+	return _btn;
+}
+
+var startBtn = new NormalBtn("start",'Start',600,250)
+var aboutBtn = new NormalBtn("about",'About',600,350)
--- js/start.js
+++ js/start.js
@@ -1,18 +1,22 @@
 
 var timer, container, params, context, events, stage;
 var canvas;
-var _width = document.body.clientWidth;
-var _height = document.body.clientHeight;
+//var _width = document.body.clientWidth;
+//var _height = document.body.clientHeight;
+var _width = 1000;
+var _height = 600;
 var marine,m2;
+var startAudio = new Audio("./audio/Heaven's Devils.mp3",true,false,true);
 window.onload = init;
 
 function init(){
 	container = Q.getDOM("container");
-	container.style.background = "-webkit-gradient(linear, 0 0, 0 bottom, from(#00889d), to(#58B000), color-stop(0.5,#94d7e1))"
-	//container.style.background = "url(img/bgImg.jpg)";
+	//container.style.background = "-webkit-gradient(linear, 0 0, 0 bottom, from(#00889d), to(#58B000), color-stop(0.5,#94d7e1))"
+	//container.style.background = "url(img/start.png)";
 	canvas = Quark.createDOM("canvas",{width:_width,height:_height});
 	container.appendChild(canvas);
 	context = new Quark.CanvasContext({canvas:canvas});
+	//context = new Q.DOMContext({canvas:container});
 	stage = new Q.Stage({
 		context:context, 
 		width:_width, 
@@ -28,7 +32,7 @@ function init(){
 	timer.start();
 	 
 	em = new Q.EventManager();
-	events = Q.supportTouch ? ["touchend"] : ["mouseup"];
+	events = Q.supportTouch ? ["touchend"] : ["mousedown","mouseup","mousemove","mouseout"];
 	em.registerStage(stage, events, true, true);
 
 	marine = new Marine({id:"marine",x:100,y:100,autoSize:true});
@@ -37,6 +41,19 @@ function init(){
 	m2.rect();
 	marine.state = 'right';
 	marine.rect();
+	var bg = new Quark.Bitmap({image:Q.getDOM("background"),rect:[0,0,1200,748],scaleX:1000/1200,scaleY:600/768});
+	stage.addChild(bg);
 	stage.addChild(marine);
 	stage.addChild(m2);
+	stage.addChild(startBtn);
+	stage.addChild(aboutBtn);
+	startAudio.play();
+	startBtn.addEventListener(events[0],function(){
+		stage.removeChild(marine);
+		stage.removeChild(m2);
+	});
+	aboutBtn.addEventListener(events[0],function(){
+		stage.addChild(marine);
+		stage.addChild(m2);
+	});
 }
